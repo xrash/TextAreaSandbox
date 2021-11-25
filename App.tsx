7deaaf4a98c5d784c3e4a11177a15a393897d610
Felipe Gonçalves <felipe@xra.sh>
2021-11-25 15:30:21 -0300

    1st commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View, PixelRatio } from 'react-native';
 
 export default function App() {
+  const fontSize = 16
+  const numberOfLines = 4
+
+  console.log('PixelRatio.getFontScale()', PixelRatio.getFontScale())
+  console.log('PixelRatio.get()', PixelRatio.get())
+  console.log('PixelRatio.getPixelSizeForLayoutSize(100)', PixelRatio.getPixelSizeForLayoutSize(100))
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ backgroundColor: 'black', height: 64 }}></View>
+
+      <View style={{ padding: 16 }}>
+        <TextInput
+          onKeyPress={(e) => {
+            console.log('onKeyPress', e.nativeEvent.key)
+          }}
+          onLayout={(e) => {
+            const { width, height } = e.nativeEvent.layout
+            console.log('onLayout', 'w', width, 'h', height)
+          }}
+          multiline
+          numberOfLines={numberOfLines}
+          style={{
+            backgroundColor: 'white',
+            textAlignVertical: 'top',
+            fontSize,
+          }}
+        />
+      </View>
+
     </View>
   );
 }
@@ -14,8 +39,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'red',
   },
 });
